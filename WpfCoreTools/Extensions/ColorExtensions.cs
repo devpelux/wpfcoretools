3f9e62b539eccc2da36f4a95576537bd4c95e545
devpelux <info@devpelux.xyz>
2021-12-28 23:38:58 +0100

    Added FromDrawingColor, and ToDrawingColor to ColorExtensions.

--- WpfCoreTools/Extensions/ColorExtensions.cs
+++ WpfCoreTools/Extensions/ColorExtensions.cs
@@ -14,5 +14,21 @@ namespace WpfCoreTools.Extensions
         /// <returns>Inverted color.</returns>
         public static Color Invert(this Color color)
             => Color.FromRgb((byte)(255 - color.R), (byte)(255 - color.G), (byte)(255 - color.B));
+
+        /// <summary>
+        /// Converts a <see cref="System.Drawing.Color"/> to a <see cref="Color"/>.
+        /// </summary>
+        /// <param name="color"><see cref="System.Drawing.Color"/> to convert.</param>
+        /// <returns><see cref="Color"/>.</returns>
+        public static Color FromDrawingColor(System.Drawing.Color color)
+            => Color.FromArgb(color.A, color.R, color.G, color.B);
+
+        /// <summary>
+        /// Converts a <see cref="Color"/> to a <see cref="System.Drawing.Color"/>.
+        /// </summary>
+        /// <param name="color"><see cref="System.Drawing.Color"/> to convert.</param>
+        /// <returns><see cref="System.Drawing.Color"/>.</returns>
+        public static System.Drawing.Color ToDrawingColor(Color color)
+            => System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
     }
 }
