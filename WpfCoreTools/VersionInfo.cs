cec4a71b703f11ada153185fe65120405677853d
devpelux <info@devpelux.xyz>
2022-05-19 20:52:09 +0200

    Added VersionInfo with version info about the package, updated target framework and project info.

---
+++ WpfCoreTools/VersionInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace WpfCoreTools
+{
+    /// <summary>
+    /// Provides version informations about the <see cref="WpfCoreTools"/> package.
+    /// </summary>
+    public static class VersionInfo
+    {
+        /// <summary>
+        /// Gets the current <see cref="WpfCoreTools"/> version code.
+        /// </summary>
+        public static string VersionCode => typeof(VersionInfo).Assembly.GetName().Version?.ToString() ?? string.Empty;
+    }
+}
